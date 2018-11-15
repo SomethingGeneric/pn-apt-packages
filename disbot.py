2f0365e01ce96da9b21bae3509c8cf7c7de7eef3
Matt C <mcompton2002@gmail.com>
2018-11-14 21:09:22 -0500

    Disbot test

---
+++ disbot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+from time import sleep
+import os
+import subprocess
+
+bot = commands.Bot(command_prefix='Alexa ', description="Alexa bot")
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command(description="Imma parrot")
+async def parrot(text=""):
+    """Imma parrot"""
+    await bot.say(text)
+
+bot.run(input("Token: "))
